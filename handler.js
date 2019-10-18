fe07bd76772b6b333f5585f109d1b5c4a8180843
Takuya KIHARA <takuya.kihara@northdetail.co.jp>
2019-10-18 19:20:05 +0900

    :sparkles: 音声操作対応

--- handler.js
+++ handler.js
@@ -1,9 +1,9 @@
-'use strict';
+'use strict'
 
-const axios = require('axios');
-const { dialogflow, HtmlResponse } = require('actions-on-google');
+const axios = require('axios')
+const { dialogflow, HtmlResponse } = require('actions-on-google')
 
-const app = dialogflow({ debug: false });
+const app = dialogflow({ debug: false })
 
 const tasksUrl = 'https://show-today-tasks.netlify.com'
 const currentListId = process.env.CURRENT_LIST_ID
@@ -12,30 +12,48 @@ const apiKey = process.env.API_KEY
 const apiToken = process.env.API_TOKEN
 
 // 起動時Intent
-app.intent('welcome', (conv) => {
-  conv.ask('今日のタスク一覧です。確認しましょう。');
-  conv.ask(new HtmlResponse({
-    url: `${tasksUrl}/?listId=${currentListId}&apiKey=${apiKey}&apiToken=${apiToken}`,
-    suppress: true
-  }));
-});
+app.intent('welcome', conv => {
+  conv.ask('今日のタスク一覧です。確認しましょう。')
+  conv.ask(
+    new HtmlResponse({
+      url: `${tasksUrl}/?listId=${currentListId}&finishedListId=${finishedListId}&apiKey=${apiKey}&apiToken=${apiToken}`,
+      suppress: true
+    })
+  )
+})
+
+app.intent('finishedTaskByVoice', async (conv, { cardNumber }) => {
+  // 声で完了にする
+  conv.ask('カードを完了にしました。')
+  conv.ask(
+    new HtmlResponse({
+      url: `${tasksUrl}/?listId=${currentListId}&finishedListId=${finishedListId}&apiKey=${apiKey}&apiToken=${apiToken}`,
+      suppress: true,
+      data: {
+        cardNumber: cardNumber
+      }
+    })
+  )
+})
 
 app.intent('finishedTask', async (conv, { cardId }) => {
   // Trelloのカードを完了に移す
-  const response = await axios.put(`https://api.trello.com/1/cards/${cardId}?idList=${finishedListId}&key=${apiKey}&token=${apiToken}&pos=bottom`)
-
-  const now = Date.now()
   conv.ask('カードを完了にしました。')
-  conv.ask(new HtmlResponse({
-    url: `${tasksUrl}/?listId=${currentListId}&apiKey=${apiKey}&apiToken=${apiToken}&d=${now}`,
-    suppress: true
-  }));
-});
+  conv.ask(
+    new HtmlResponse({
+      url: `${tasksUrl}/?listId=${currentListId}&finishedListId=${finishedListId}&apiKey=${apiKey}&apiToken=${apiToken}`,
+      suppress: true,
+      data: {
+        cardId: cardId
+      }
+    })
+  )
+})
 
-app.fallback((conv) => {
-  console.log('Default Fallback Intent');
-  console.log(conv);
-  conv.ask('フォールバック');
-});
+app.fallback(conv => {
+  console.log('Default Fallback Intent')
+  console.log(conv)
+  conv.ask('フォールバック')
+})
 
-module.exports.fulfillment = app;
+module.exports.fulfillment = app
